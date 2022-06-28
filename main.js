1cddfebad54cf379a115ff4a6a5c1b9760e5ffef
clayton wright <claytonwright@claytons-MacBook-Pro.local>
2022-06-28 12:59:10 -0400

    completed.

---
+++ main.js
@@ -0,0 +1,19 @@
+let para = document.querySelector('p');
+
+para.style.color = 'lightblue';
+
+let header1 = document.querySelector('h1');
+
+header1.style.fontsize = "48px";
+
+para.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = 0.5;
+
+let item3 = document.querySelector('#item-3');
+item3.innerText = "I say, \"Hi!\"";
+
+let image = document.querySelector('img');
+image.src = "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+image.style.height = "300px";
